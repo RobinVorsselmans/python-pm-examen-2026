0a3e7950163f4511eef42b693d8e57d30e3ea868
RobinVorsselmans <robinvorsselmans1@hotmail.com>
2026-05-27 20:07:52 +0200

    uncommented _validate_integers stap 4

--- stringcalc.py
+++ stringcalc.py
@@ -27,7 +27,7 @@ def _extract_integers(expr: str) -> list[int]:
 
 
 def _validate_integers(integers: list[int]) -> None:
-    # negatives = [str(i) for i in integers if i < 0]
-    # if negatives:
-    #     raise ValueError(f"negatives not allowed: {' '.join(negatives)}")
+    negatives = [str(i) for i in integers if i < 0]
+    if negatives:
+        raise ValueError(f"negatives not allowed: {' '.join(negatives)}")
     return
